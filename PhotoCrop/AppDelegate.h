9bdf71f7576907088bc1d3579c3c640ad0dc9bba
liudukun <liudukun@126.com>
2019-06-09 16:23:24 +0800

    Initial Commit

---
+++ PhotoCrop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoCrop
+//
+//  Created by liudukun on 2019/6/9.
+//  Copyright © 2019 liudukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoCrop/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PhotoCrop
+//
+//  Created by liudukun on 2019/6/9.
+//  Copyright © 2019 liudukun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
